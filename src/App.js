0b74528c64e8e7036fda765e93fdf5372ab5621c
bjmontillon <pangilinanbj@gmail.com>
2021-07-25 23:33:38 +0800

    updated path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './App.css';
-import { select } from 'd3'
+import { select, line, curveCardinal } from 'd3'
 
 
 
@@ -14,21 +14,33 @@ function App() {
   useEffect(() => {
 
     const svg = select(svgRef.current);
+    const myLine = line()
+        .x((value, index) => index * 50)
+        .y(value => 150 - value)
+        .curve(curveCardinal);
+        svg.selectAll('path')
+        .data([data])
+        .join('path')
+        .attr('d', value => myLine(value))
+        .attr('fill', 'none')
+        .attr('stroke', 'blue');
 
-    svg.selectAll('circle')
-        .data(data)
-        .join('circle')
-        .attr('r', value => value)
-        .attr('cx', value => value * 2)
-        .attr('cy', value => value * 2)
-        .attr('stroke', 'red');
+    // svg.selectAll('circle')
+    //     .data(data)
+    //     .join('circle')
+    //     .attr('r', value => value)
+    //     .attr('cx', value => value * 2)
+    //     .attr('cy', value => value * 2)
+    //     .attr('stroke', 'red');
 
     console.clear()
     
   }, [data])
   return (
     <React.Fragment>
-    <svg ref={ svgRef }></svg>
+    <svg ref={ svgRef }>
+      <path d='' stroke='red' fill='none' />
+    </svg>
     <br />
     <button onClick={() => setData(data.map(value => value + 5))}>
     Update data
